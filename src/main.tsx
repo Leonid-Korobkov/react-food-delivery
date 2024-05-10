64783f6e7a65261c850e328439f4eb38559b2075
Leonid-Korobkov <korobkov-leo@mail.ru>
2024-05-10 19:12:48 +0300

    added suspense and loader

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, defer } from 'react-router-dom'
 import Cart from './pages/Cart/Cart'
 import Error from './pages/Error/Error'
 import Layout from './layout/Menu/Menu'
 import Product from './pages/Product/Product'
 import axios from 'axios'
 import { baseUrl } from './helpers/API'
+import Loader from './components/Loader/Loader'
 
 const Menu = lazy(() => import('./pages/Menu/Menu'))
 
@@ -23,7 +24,7 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: (
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<Loader />}>
             <Menu />
           </Suspense>
         )
@@ -33,15 +34,22 @@ const router = createBrowserRouter([
         element: <Product />,
         errorElement: <Error />,
         loader: async ({ params }) => {
-          const { data } = await axios.get(`${baseUrl}/products/${params.id}`)
-          return data
+          return defer({
+            data: new Promise((resolve, reject) => {
+              setTimeout(() => {
+                axios
+                  .get(`${baseUrl}/products/${params.id}`)
+                  .then((data) => resolve(data))
+                  .catch((e) => reject(e))
+              }, 0)
+            })
+          })
+          // axios.get(`${baseUrl}/products/${params.id}`).then(data => data)
+          // const { data } = await axios.get(`${baseUrl}/products/${params.id}`)
+          // return data
         }
       }
     ]
-  },
-  {
-    path: '/cart',
-    element: <Cart />
   },
   {
     path: '*',
--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,11 +1,17 @@
-import { useLoaderData } from 'react-router-dom'
+import { Await, useLoaderData } from 'react-router-dom'
 import st from './Product.module.css'
 import { IProduct } from '../../interfaces/IProduct'
+import { Suspense } from 'react'
+import Loader from '../../components/Loader/Loader.tsx'
 
 function Product() {
-  const data = useLoaderData() as IProduct
+  const data = useLoaderData() as { data: IProduct }
 
-  return <div className={st.product}>Product - {data.name}</div>
+  return (
+    <Suspense fallback={<Loader />}>
+      <Await resolve={data.data}>{({ data }: { data: IProduct }) => <div className={st.product}>Product - {data.name}</div>}</Await>
+    </Suspense>
+  )
 }
 
 export default Product
